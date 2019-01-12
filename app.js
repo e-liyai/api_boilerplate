432a25e68d164bf6d2fe857f34827a8e801e73a6
Andela-eugene <eugene.liyai@andela.com>
2019-01-12 20:35:44 +0300

    [Feat: #] add put route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const volleyball = require('volleyball');
-// const bodyParser = require('body-parser');
+const users = require('./users')
 
 const app = express();
 
@@ -8,8 +8,31 @@ app.use(volleyball)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', function(req, res, next) {
-  res.send('hi!')
+app.get('/users', function(req, res, next) {
+  res.send(users)
+})
+
+app.get('/user/:id', function (req, res, next) {
+  const id = req.params.id
+  const reqQuery = req.query
+  const user = users[id]
+  const response = {}
+  Object.keys(reqQuery).forEach(function(key) {
+     response[key] = user[key]
+  })
+  res.send(response)
+})
+
+app.post('/users', function (req, res, next) {
+  const user = req.body
+  users.push(user)
+  res.send(user)
+})
+
+app.put('/users/:id', function(req, res, next) {
+  const user = users[req.params.id]
+  Object.assign(user, req.body)
+  res.send(user)
 })
 
 const server = app.listen(4000, function() {
---
+++ users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'john doe',
+    age: 44,
+    location: 'london'
+  },
+  {
+    name: 'jane doe',
+    age: 32,
+    location: 'mexico'
+  },
+  {
+    name: 'foo bar',
+    age: 21,
+    location: 'oslo'
+  },
+  {
+    name: 'mark one',
+    age: 11,
+    location: 'ohio'
+  }
+]
