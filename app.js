e69e7749039c0a315e251ebf3b46b3822c998378
Andela-eugene <eugene.liyai@andela.com>
2019-01-12 16:44:12 +0300

    [Feat: #] add nodemon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(volleyball())
+app.use(volleyball)
+app.use(bodyParser)
 
 const server = app.listen(4000, function() {
   console.log('Listening on port ', server.address().port)
